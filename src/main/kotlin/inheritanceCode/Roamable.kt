f80d79685367bf5e555cf81d1bb2157f5322b0ff
Janet <janetmutua04@gmail.com>
2023-02-20 08:30:01 +0300

    Feat: Interfaces and polymorphism

--- src/main/kotlin/inheritanceCode/Roamable.kt
+++ src/main/kotlin/inheritanceCode/Roamable.kt
@@ -5,6 +5,8 @@ interface Roamable {
 
     var roamName: String
         get() = "Roaming?"
+        //can't use the field keyword in properties in an interface
+        //since an interface has no backing fields for properties(holds no reference)
         set(value){
             println("We don't roam here.")
         }
--- src/main/kotlin/inheritanceCode/Vehicle.kt
+++ src/main/kotlin/inheritanceCode/Vehicle.kt
@@ -10,6 +10,29 @@ class Vehicle : Roamable{
 
 
 fun main(args: Array<String>){
-    val car1 = Vehicle()
-    println(car1.roam())
+    //polymorphism
+    val listRoamables = arrayOf(Vehicle(), Hippo("twenty", "Hippo"), lionKing("sixteen", "Lion")
+    )
+
+    for(item in listRoamables){
+        println(item.roam())
+
+        if (item is Animal){
+            item.eat()
+        }
+
+        var animalType = if (item is Animal)"is Animal" else "is not animal"
+        println(animalType)
+
+
+        var noiseSounds = when (item){
+            is Hippo -> println(item.makeNoise())
+            is lionKing -> println(item.makeNoise())
+            else -> println(item.roam())
+        }
+
+        println(noiseSounds)
+    }
+
+
 }
