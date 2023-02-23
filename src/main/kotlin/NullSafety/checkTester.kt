4a3ae424280d3c08f3a825b7ce768c23591c7269
Janet <janetmutua04@gmail.com>
2023-02-23 08:38:10 +0300

    Feat:safe calls

---
+++ src/main/kotlin/NullSafety/checkTester.kt
@@ -0,0 +1,56 @@
+package NullSafety
+
+class checkTester (var catName:String? = ""){
+    fun Meow(){
+        println("Meow!")}
+}
+
+class Wolf(val name:String?){
+    fun eat(){
+        println("I eat meat!")
+    }
+}
+
+fun wolfDiary():Wolf?{
+    return Wolf("Jaguar")
+}
+
+fun main(args:Array<String>){
+    val myCats = arrayOf(
+        checkTester("Rosie"),
+       null,
+        checkTester("Misty")
+    )
+
+    for (cat in myCats){
+        cat?.let{
+            print("${it.catName}:")
+            it.Meow()
+        }
+
+    }
+
+    //holds the return value of the wolf diary function
+    // this function returns an instance of the Wolf class
+
+    //Implementation 1:
+    //var myWolf = wolfDiary()
+    //myWolf?.eat()
+
+    //Implementation 2:
+
+    wolfDiary()?.let {
+        it.eat()
+    }
+
+
+    //ELvis operator
+
+    wolfDiary()?.let { it.eat() } ?: -1
+
+    //Not null assertion operator
+
+    //var water = null
+    //var z = water!!
+
+}
--- src/main/kotlin/NullSafety/nullables.kt
+++ src/main/kotlin/NullSafety/nullables.kt
@@ -6,4 +6,28 @@ class nullables {
     val myArray: Array<String?> = arrayOf("New Zealand", "Paris", null)
     var myString: String? = null
     var z: Any? = Any()
+
+    fun checkNull(){
+        println("is not null")
+    }
+}
+
+class confirmNullValue{
+    var value:nullables? = nullables()
+}
+
+fun main(args:Array<String>){
+    var nullChecker: confirmNullValue? = confirmNullValue()
+
+    //using safe calls to access properties and functions in an instance that is nullable
+    //safe call chain
+    nullChecker?.value?.checkNull()
+
+    var checkArray = nullables()
+    for (item in checkArray.myArray){
+        item?.let {
+            println(it)
+        }
+    }
+
 }
