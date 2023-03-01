c6150c4f0d1e2be376c6015cb4da34f2497e17b8
Janet <janetmutua04@gmail.com>
2023-03-01 12:46:00 +0300

    updates

--- src/main/kotlin/Collections/collectionsKotlin.kt
+++ src/main/kotlin/Collections/collectionsKotlin.kt
@@ -1,6 +1,29 @@
 package Collections
 
 fun main(args:Array<String>){
-    var nullableArray : Array<String?> = arrayOfNulls(4)
+    usableMethods()
 
 }
+
+fun usableMethods(){
+    var nullableArray : Array<String?> = arrayOfNulls(4)
+
+//    Lists
+
+    var myRecipe = listOf("Bread", "Eggs", "Black pepper")
+
+    println(myRecipe.get(2))
+
+    println(myRecipe.indexOf("Eggs"))
+
+    var myChangeableRecipe = mutableListOf("Potatoes", "Garlic", "Carrots")
+    myChangeableRecipe.add("Onions")
+
+    if (myChangeableRecipe.contains("Garlic")){
+        myChangeableRecipe.remove("Garlic")
+        myChangeableRecipe.add("Beef")
+        myChangeableRecipe.removeAt(1)
+        println(myChangeableRecipe)
+    }
+}
+
