dbd9e1efd180aa749a1c111c14d5bfeba7ff0a96
Janet <janetmutua04@gmail.com>
2023-03-03 08:40:27 +0300

    Hashcodes and equality

--- src/main/kotlin/Collections/CreatingSets.kt
+++ src/main/kotlin/Collections/CreatingSets.kt
@@ -7,16 +7,40 @@ fun main(args:Array<String>){
     println(myFriends)
 
     similarityCheck()
+    mutableSetFunc()
 }
 
+data class Furniture(val type:String, val material:String)
 fun similarityCheck(){
-    val x = "Chair"
-    val y = "Chair"
+    val x = Furniture("Chair", "wood")
+    val y = Furniture("Chair", "wood")
     val z = x
 
     println(x.hashCode())
     println(y.hashCode())
     println(z.hashCode())
-    println("$y === $x")
-    println("$y === $z")
+    println("${y === x}")
+    println("${y == x}")
+    println("${y === z}")
+}
+
+fun mutableSetFunc(){
+    val ySet = mutableSetOf("Loise", "Crisi", "John", "James")
+//    copying to a different set
+    val xSet = ySet.toSet()
+    println("mutable set section")
+    ySet.add("Karey")
+//    copying to a list
+    val myNewList = ySet.toMutableList()
+    myNewList.add("Loise")
+    println("my list is : $myNewList")
+
+    if (myNewList.size > myNewList.toSet().size){
+        println("Check for duplicates")
+    }
+    println(ySet)
+    println(xSet)
+
+    val myNewArray = ySet.toTypedArray()
+    println(myNewArray)
 }
---
+++ src/main/kotlin/Collections/TrialCode.kt
@@ -0,0 +1,20 @@
+package Collections
+
+fun main(args:Array<String>){
+    val set = setOf( Duck(), Duck(17))
+
+    println(set)
+}
+
+class Duck(val size:Int = 17){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other is Duck && size == other.size)return true
+
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return size
+    }
+}
