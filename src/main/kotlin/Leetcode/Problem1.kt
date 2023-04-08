505570aaa521ad150ff84bd03ed314997cd7c252
Janet <janetmutua04@gmail.com>
2023-04-08 07:04:44 +0300

    Leetcode challenge

--- src/main/kotlin/Leetcode/Problem1.kt
+++ src/main/kotlin/Leetcode/Problem1.kt
@@ -2,24 +2,27 @@ package Leetcode
 
 fun main(args:Array<String>){
     val solutionOne = Solution()
-    solutionOne.numRescueBoats(intArrayOf(2,2), 6)
-    //solutionOne.numRescueBoats(intArrayOf(3, 2, 2,1), 3)
+    val solutionTwo = Solution()
+    //solutionOne.numRescueBoats(intArrayOf(2,2), 6)
+    //solutionTwo.numRescueBoats(intArrayOf(3, 2, 2,1), 3)
+    solutionTwo.numRescueBoats(intArrayOf(3, 1, 7), 7)
 
 }
 class Solution {
-    fun numRescueBoats(people: IntArray, limit: Int): Int {
+    fun numRescueBoats(people: IntArray, limit: Int){
         var boat = 0
         var totalBoatWeight = 0
         var count = 0
         var balance = 0
+        var loopCount = 0
         people.sort()
 
-        var lastElement = people[people.lastIndex]
-        var secondLastElement = people[people.lastIndex - 1]
+        var lengthOfArray = people.size
 
 
         for(personWeight in people){
-            println("Person weight: $personWeight")
+            loopCount ++
+            println("we are in $personWeight weight")
             if(personWeight <= limit && totalBoatWeight <= limit){
                 balance = limit - totalBoatWeight
                 if(balance == 0){
@@ -30,33 +33,43 @@ class Solution {
                     totalBoatWeight += personWeight
                     if (totalBoatWeight == limit){
                         boat ++
-                    } else if (totalBoatWeight < limit && lastElement == personWeight) {
+                    } else if (totalBoatWeight < limit && loopCount == lengthOfArray) {
+                        println("We have looped $loopCount times")
 
-                        if (lastElement == secondLastElement){
                             boat++
+                    } else if (totalBoatWeight < limit){
+                        count ++
+                        println("My total count is $count")
+                        continue
+                    } else {
+                        if (count != 0){
+                            count ++
+                            boat += count
+                        } else{
+                            println("what")
+                            boat ++
                         }
 
 
-                        println(people.lastIndex)
-                        println(people[people.lastIndex])
-
-                    } else {
-                        count ++
-                        println("what")
-                        continue
                     }
 
                 }else {
-                    count ++
-                    boat += count
-                    totalBoatWeight = 0
-                    count = 0
+                    totalBoatWeight += personWeight
+                    println("Total Weight: $totalBoatWeight")
+                    if (count != 0 && totalBoatWeight > limit){
+                        boat += count
+                        totalBoatWeight = 0
+                        count = 0
+                    }else{
+                        boat++
+                    }
+
                 }
             }
         }
 
 
-        return boat
+        println("No of boats: $boat")
 
     }
 }
